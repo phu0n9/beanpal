8b3ee487033674a466fe6c0f8f7cc3e602c2e31e
Phuong Nguyen <s0595566@htw-berlin.de>
2024-12-08 03:30:39 +0100

    Initial commit

---
+++ client/src/constant.ts
@@ -0,0 +1,4 @@
+export const ZOOM_SETTINGS = {
+    SDK_KEY: process.env.REACT_APP_ZOOM_SDK ?? 'PaGiF_7KSf22LMj_CfHqsg',
+    AUTH_ENDPOINT: process.env.REACT_APP_AUTH_ENDPOINT ?? 'http://localhost:6000',
+}
